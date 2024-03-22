b49ec1bc54d26241e4a8b0f07577693d2a2e64e1
alphamoz <jasonzzz.jeremy@gmail.com>
2024-03-22 13:55:10 +0700

    fix: sensor data

--- Test_Pymavlink_Plot.py
+++ Test_Pymavlink_Plot.py
@@ -53,30 +53,40 @@ boot_time = time.time()
 # Wait a heartbeat before sending commands
 master.wait_heartbeat()
 
-alt= lon= lat = 0
+depth = alt= lon= lat = 0
 
 while True:
-    msg = master.recv_match()
-    if msg is not None:
-        if msg.get_type() == 'GLOBAL_POSITION_INT':
-            # Get latitude and longitude in degrees
-            lat = msg.lat / 1e7
-            lon = msg.lon / 1e7
-            # Altitude in meters
-            alt = msg.alt / 1e3
-            print("Position (Lat, Lon, Alt):", lat, lon, alt)
+    msg_attitude = master.recv_match(type='ATTITUDE', blocking=False)
+    msg_depth = master.recv_match(type='VFR_HUD', blocking=False)
+    msg_position = master.recv_match(type='GLOBAL_POSITION_INT', blocking=False)
+    if msg_attitude and msg_depth and msg_position is not None:
+            roll = msg_attitude.roll
+            pitch = msg_attitude.pitch
+            yaw = msg_attitude.yaw 
+            depth = msg_depth.alt 
+            lat = msg_position.lat
+            lon= msg_position.lon
+            alt = msg_position.alt
+            # print(roll,pitch,yaw,depth)
+        # if msg.get_type() == 'GLOBAL_POSITION_INT':
+        #     # Get latitude and longitude in degrees
+        #     lat = msg.lat / 1e7
+        #     lon = msg.lon / 1e7
+        #     # Altitude in meters
+        #     alt = msg.alt / 1e3
+        #     print("Position (Lat, Lon, Alt):", lat, lon, alt)
             
-        elif msg.get_type() == 'ATTITUDE':
-            # Roll, pitch, and yaw in radians
-            roll = msg.roll
-            pitch = msg.pitch
-            yaw = msg.yaw
-            print("Attitude (Roll, Pitch, Yaw):", roll, pitch, yaw)
+        # elif msg.get_type() == 'ATTITUDE':
+        #     # Roll, pitch, and yaw in radians
+        #     roll = msg.roll
+        #     pitch = msg.pitch
+        #     yaw = msg.yaw
+        #     print("Attitude (Roll, Pitch, Yaw):", roll, pitch, yaw)
             
-        elif msg.get_type() == 'VFR_HUD':
-            # Altitude above ground in meters
-            alt_agl = msg.alt
-            print("Altitude AGL:", alt_agl)
-        plot(alt, lat, lon)
+        # elif msg.get_type() == 'VFR_HUD':
+        #     # Altitude above ground in meters
+        #     alt_agl = msg.alt
+            # print("Altitude AGL:", alt_agl)
+    plot(depth, lat, lon)
     time.sleep(1)
   
