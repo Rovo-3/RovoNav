c0b086d1952368fcea40dd4f094009b7a5d0196c
kayynnn <bagasnanda@gmail.com>
2024-04-17 09:05:32 +0700

    Fixed: delays; Added: Battery Status; Edit: Disarm to Slave2

--- FailsafeSocket/LatestScript/NewSlave.py
+++ FailsafeSocket/LatestScript/NewSlave.py
@@ -6,105 +6,88 @@ from pymavlink import mavutil
 import sys
 
 slave = mavutil.mavlink_connection('udpin:0.0.0.0:14445')
+slave2 = mavutil.mavlink_connection('udpin:0.0.0.0:14552')
+slave2.wait_heartbeat()
 slave.wait_heartbeat()
 
-# Set the port
-bind_ip = '192.168.2.2'
+bind_ip = '192.168.2.2'  # Set IP to Companion IP
 bind_port = 2345
 print("Port set to:", bind_port)
-GCS_ID = 250
+GCS_ID = 255  # GCS ID must be 255, otherwise controller will not work
+
 
 def SendHB():
     while True:
         hb = slave.recv_match(type="HEARTBEAT", blocking=True)
+        bs = slave.recv_match(type="SYS_STATUS", blocking=True)
         converted_hb = hb.to_dict()
-        # print(slave.source_system)
 
-        # print(hb.get_srcSystem())
-        # print(hb)
-        # print("Param ID: %s" % hb.param_id)
-        # print("Message type: ", hb.get_type())
         json_data = json.dumps(converted_hb)
+
         if hb.get_srcSystem() == GCS_ID:
-            return hb.get_srcSystem(), json_data
+            return hb.get_srcSystem(), json_data, bs
 
-    # sendhb = slave.mav.heartbeat_send(
-    #     9,     # MAVTYPE = MAV_TYPE_GCS
-    #     8,      # MAVAUTOPILOT = MAV_AUTOPILOT_INVALID
-    #     191,    # MAV_MODE = MAV_MODE_FLAG_SAFETY_ARMED, have also tried 0 here
-    #     0,0)    # MAVSTATE =
 
-    # if sendhb:
-    #     print("FAULTY SENDING HB")
-timeout=0
-armed_permission=True
+timeout = 0
+armed_permission = True
+
 
 def reset_timeout():
     global timeout, armed_permission
-    timeout=0
-    armed_permission=True
+
+    timeout = 0
+    armed_permission = True
+
 
 def count_timeout(connection):
-    global armed_permission,timeout
+    global armed_permission, timeout
+
     if armed_permission:
-        timeout+=1
-    if timeout>5 and armed_permission:
+        timeout += 1
+
+    if timeout > 4 and armed_permission:
         connection.arducopter_disarm()
         print("Vehicle DISARMED")
-        armed_permission=False
+        armed_permission = False
+
 
 try:
     while True:
-        # Connect to the server
         try:
-            # Socket object
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print("SEARCHING NEW MASTER")
 
-            # Make connection
             s.connect((bind_ip, bind_port))
             print("MASTER FOUND")
 
             while True:
                 print("\nSLAVE: ")
-                
+
                 try:
-                    QGC_ID, hb = SendHB()
+                    QGC_ID, hb, bs = SendHB()
                     if QGC_ID == GCS_ID:
-                        # hb = json.loads(hb)
-                        # print("Base Mode: ", hb['base_mode'])
-                        # s.send('PING'.encode())
-                        # s.send(str(hb).encode())
                         print(hb)
+                        print(bs)
                         s.sendall(bytes(hb, encoding="utf-8"))
-                        
-                    
-                    # Receive data from the server
+
                     server_response = s.recv(1024).decode()
-                    
-                    # if hb.param_id == "base_mode":
-                    #     print("YEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-                    
+
                     if server_response == "ACK":
-                        # send data to server
                         print("Master Says, OK and VERIFFIED")
                         print(hb)
                         reset_timeout()
-            
+
                     else:
                         raise Exception('Master Didnt Give Permission!')
-                    
+
                     print(f"Received from server: {server_response}")
 
-                # except KeyboardInterrupt:
-                #     sys.exit(1)
                 except:
                     reset_timeout()
                     s.close()
                     print("Connection disconnected")
                     print("FREE FROM SLAVERY")
                     break
-                
 
                 time.sleep(1)
 
@@ -112,8 +95,6 @@ try:
             count_timeout(slave)
             s.close()
             time.sleep(1)
-            # break # <------------------------------------------------------ i changed this, pls change it back to pass
+
 except KeyboardInterrupt:
     s.close()
-    # sys.exit()
-# slave.arducopter_disarm()
