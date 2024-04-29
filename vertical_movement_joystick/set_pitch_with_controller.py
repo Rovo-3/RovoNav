3652cb8aee8d10db69990a566b8213a640e38560
alphamoz <jasonzzz.jeremy@gmail.com>
2024-04-29 17:19:21 +0700

    feat: trigger key for vertical movement

--- vertical_movement_joystick/set_pitch_with_controller.py
+++ vertical_movement_joystick/set_pitch_with_controller.py
@@ -19,7 +19,6 @@ class MySub:
         self.master=master_connection
         self.boot_time = boot_time
         self.is_armed = False
-        # return self.master
 
     def wait_heartbeat(self):
         self.master.wait_heartbeat()
@@ -99,6 +98,12 @@ class My_joystick:
         self.pitch_desired = pitch_desired
         self.pitch_changes = 0
         self.status="HOLD"
+        self.is_shift_key = False
+        # you can modify the shift key and the combination key for vertical mode activation
+        # those key will change the value of activate_vertical_mode
+        self.shift_key = 10
+        self.vertical_mode_key = 1
+        self.activate_vertical_mode = False
     def print_add(self, joy):
         print('Added', joy)
 
@@ -107,6 +112,51 @@ class My_joystick:
 
     def key_received(self, key):
         # please add the the desired_yaw modifier
+        # checking shift key
+        self.check_shift(key.keytype, key.number, key.value)
+        print("Checking shift key status: ", self.is_shift_key)
+
+        if self.is_shift_key == True:
+        #     # wait for the shift status
+            print("Checking the combination")
+            self.check_combination(key.keytype, key.number, key.value)
+        print("Does vertical mode active? ", self.activate_vertical_mode)
+
+        if self.activate_vertical_mode:
+            self.vertical_operation(key)
+        
+        # if key.keytype =="-Axis":
+        print('received', key)
+        print('key type', key.keytype)
+        print('key value', key.value)
+        print('key number', key.number)
+    def update_yaw(self):
+        self.yaw_desired+=self.yaw_changes
+        if self.yaw_desired>=360:
+            self.yaw_desired-=360
+        if self.yaw_desired<0:
+            self.yaw_desired+=360
+        return self.yaw_desired
+    def update_pitch(self):
+        self.pitch_desired += self.pitch_changes
+        if self.pitch_desired>110:
+            self.pitch_desired=110
+        if self.pitch_desired<-110:
+            self.pitch_desired=-110
+        return self.pitch_desired
+    def check_shift(self, type, number, value):
+        if type == "Button" and number == self.shift_key and value == 1:
+            self.is_shift_key=True
+        elif type == "Button" and number == self.shift_key and value == 0: 
+            self.is_shift_key=False
+        elif type == "Button" and number != self.shift_key and value == 1:
+            self.activate_vertical_mode=False
+    def check_combination(self, type, number, value):
+        if type == "Button" and number == self.vertical_mode_key and value == 1:
+            self.activate_vertical_mode=not(self.activate_vertical_mode)
+        elif type =="Button" and number != self.vertical_mode_key and value ==1:
+            self.activate_vertical_mode=False
+    def vertical_operation(self, key):
         max_changes = 5
         if key.keytype == "Axis":
             if key.number == 0:
@@ -138,63 +188,43 @@ class My_joystick:
             if key.number == 2 or key.number == 5:
                 if abs(key.value)<0.2:
                     self.status = "HOLD"
-        # if key.keytype =="-Axis":
-        print('received', key)
-        print('key type', key.keytype)
-        print('key value', key.value)
-        print('key number', key.number)
-    def update_yaw(self):
-        self.yaw_desired+=self.yaw_changes
-        if self.yaw_desired>360:
-            self.yaw_desired-=360
-        if self.yaw_desired<-360:
-            self.yaw_desired+=360
-        return self.yaw_desired
-    def update_pitch(self):
-        self.pitch_desired += self.pitch_changes
-        if self.pitch_desired>110:
-            self.pitch_desired=110
-        if self.pitch_desired<-110:
-            self.pitch_desired=-110
-        return self.pitch_desired
+
 
 if __name__ == "__main__":
-    # setting up sub
+    # setting up sub mavlink connection
     master = mavutil.mavlink_connection('tcp:127.0.0.1:5762')
     # master = mavutil.mavlink_connection('udp:0.0.0.0:12346')
     boot_time = time.time()
+    # instantiation for joystick event and for the ROV
     joystick_event = My_joystick()
     my_sub = MySub(master_connection=master, boot_time=boot_time)
     my_sub.wait_heartbeat()
-    # my_sub.arming()
+    # make a thread for the joystick event
     thread1 = threading.Thread(target=run_event_loop, args=(joystick_event.print_add, joystick_event.print_remove, joystick_event.key_received,))
-    # thread2 = threading.Thread(target=otherloop, args=(stop_event,))
     thread1.start()
-    # thread2.start()
-    my_sub.change_mode("ALT_HOLD")
+
     try:
         while True:
             print("here we are on main loop")
-            # ADD PITCH CONTROL 90 DEGREE
-            # update the desired yaw value
-            pitch_desired = joystick_event.update_pitch()
-            yaw_desired = joystick_event.update_yaw()
-            roll_desired = 0
-            my_sub.set_target_attitude(roll_desired, pitch_desired, yaw_desired)
-
-            print("desired rpy :", roll_desired,pitch_desired,yaw_desired)
-            # ADD YAW DESIRED CHANGE BY JOYSTICK
-            time.sleep(0.5)
-            # if(joystick_event.status == "PITCH_UP"):
-            #     my_sub.set_rc_channel_pwm(1,1600)
-            # elif(joystick_event.status == "PITCH_DOWN"):
-            #     my_sub.set_rc_channel_pwm(1,100)
-            # else:
-            #     my_sub.set_rc_channel_pwm(1,1500)
-            # print(joystick_event.status)
+            # This mode is trigerred by using shift key (button on the center, key number 10) with B key (key number 1)
+            if joystick_event.activate_vertical_mode:
+                # Changing pitch and yaw desired
+                my_sub.change_mode("ALT_HOLD")
+                pitch_desired = joystick_event.update_pitch()
+                yaw_desired = joystick_event.update_yaw()
+                roll_desired = 0
+                # Setting the rpy by the desired setpoint on default 0,90,0
+                my_sub.set_target_attitude(roll_desired, pitch_desired, yaw_desired)
+                print("desired rpy :", roll_desired,pitch_desired,yaw_desired)
+                time.sleep(0.5)
+                # passing next code and go back to the new loop
+                pass
+            # sleep if the combination of vertical mode is not triggered
+            time.sleep(1)
+            print("Waiting for the custom mode command")
     except KeyboardInterrupt:
         print("KeyboardInterrupt detected, stopping threads...")
-        # stop_event.stop()
+        # Disarming 
         my_sub.disarming()
         os._exit(1)
         # print("All threads have finished.")
